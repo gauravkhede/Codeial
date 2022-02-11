b83e078cd66d490137106b2ebf295b8255b0530f
Gaurav Khede <gauravkhede1@gmail.com>
2022-02-11 20:28:28 +0530

    setting up a view for home controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,7 @@
 module.exports.home=function(req,res){
-    res.end('<h1> Express is up for codeial </h1>');
+    // res.end('<h1> Express is up for codeial </h1>');
+    res.render('home',{
+        title:'Home'
+    });
 }
 //module.ecports.home=function(); to export this controller outside this file
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port=8000;
 //use express router
 app.use('/',require('./routes'));
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 
 
 
